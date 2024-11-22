6d0813fe883ad63aabde3da4d82a5e408d7381c8
oblivion4us <oblivion4us@icloud.com>
2024-11-22 15:55:43 +0200

    add start code for database

--- onepointfivedj/news/models.py
+++ onepointfivedj/news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=200)
+    short_desription = models.CharField('Краткое описание', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('Дата публикации')
+    author = models.CharField('Автор', max_length=70)
